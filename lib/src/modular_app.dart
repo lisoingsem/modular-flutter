5111b59661a329391365b560bd3222e14b53e794
Lisoing Sem <lisoingsem@gmail.com>
2025-11-25 15:57:42 +0700

    Format initialRoute guard

--- lib/src/modular_app.dart
+++ lib/src/modular_app.dart
@@ -398,10 +398,13 @@ class _ModularAppState extends State<ModularApp> {
             useMaterial3: true,
           ),
       routes: routes,
-      initialRoute: widget.initialRoute != null && routes.containsKey(widget.initialRoute)
-          ? widget.initialRoute
-          : null,
-      home: routes.isEmpty || (widget.initialRoute != null && !routes.containsKey(widget.initialRoute))
+      initialRoute:
+          widget.initialRoute != null && routes.containsKey(widget.initialRoute)
+              ? widget.initialRoute
+              : null,
+      home: routes.isEmpty ||
+              (widget.initialRoute != null &&
+                  !routes.containsKey(widget.initialRoute))
           ? (widget.home ??
               const Scaffold(body: Center(child: Text('No routes configured'))))
           : null,
